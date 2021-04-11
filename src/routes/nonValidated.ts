0d4648d41905781678f213ff2217684640a3b9e4
Bruno R.Novais <novais.rezende@gmail.com>
2021-04-10 21:58:01 -0300

    :ambulance: NonValidated routes and password verify
    improved password verify and updated incorret  nonvalidated routes

--- src/routes/nonValidated.ts
+++ src/routes/nonValidated.ts
@@ -1,12 +1,21 @@
 import { Router, Request, Response } from 'express';
 import { LoginController } from '../controllers/loginController';
+import { UserController } from '../controllers/userController';
 
 const loginController = new LoginController();
+const userController = new UserController();
 
 const routes = Router();
 
 routes.post('/api/login', (req: Request, res: Response) =>
   loginController.login(req, res)
+);
+
+routes.get('/api/users/', (req: Request, res: Response) =>
+  userController.findAll(req, res)
+);
+routes.post('/api/users', (req: Request, res: Response) =>
+  userController.create(req, res)
 );
 
 export default routes;
